232d9bd01299b4ef5a052a103edab44180e01e57
omcal <omerisildak0@gmail.com>
2024-10-18 05:35:58 +0300

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
+#include <map>
 
 int main() {
   // Flush after every std::cout / std:cerr
+  std::map<std::string, int> mp;
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-
   std::cout << "$ ";
   std::string input;
   std::getline(std::cin,input);
-  // std::string input;
-  // std::getline(std::cin, input);
+  std::map<std::string, int>::iterator it = mp.begin();
+  while (it != mp.end()) {
+        if (it->first== input){
+            // Do stuff
+        }
+  }
+  std::cout<<input<<": command not found"<<std::endl;
+
+
+
 }
