02a139a2e5582472d19b965f2f6c8e44b7bc761a
omcal <omerisildak0@gmail.com>
2024-10-18 06:17:47 +0300

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <map>
+#include <sstream>
+#include <vector>
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -9,12 +11,28 @@ int main() {
         std::cerr << std::unitbuf;
         std::cout << "$ ";
         std::string input;
+        std::vector<std::string> commands;
+        std::string  word;
+
         std::getline(std::cin,input);
+        std::stringstream ss(input);
+
+         while (ss>>word){
+            commands.push_back(word);
+        }
         if (input=="exit 0"){
             exit(0);
         }
-        std::cout<<input<<": command not found"<<std::endl;
 
+        if (commands[0]=="echo"){
+
+            for (int i = 1; i <commands.size()-1 ; ++i) {
+                std::cout<<commands[i]<<" ";
+            }
+            std::cout<<commands[commands.size()-1]<<std::endl;
+        }else{
+            std::cout<<input<<": command not found"<<std::endl;
+        }
     }
 
 }
